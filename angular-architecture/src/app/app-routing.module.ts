3ec9c557c138810d6a5eb8e9defc826b2901816a
unknown <gokussjindia@gmail.com>
2024-06-12 23:19:12 +0530

    Page not found component added

--- angular-architecture/src/app/app-routing.module.ts
+++ angular-architecture/src/app/app-routing.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent },
+  { path: '', redirectTo: '/login' },
   { path: '/login', component: LoginComponent },
   {
     path: '/home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
-  }, //modular routing
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent,
+  },
 ];
 
 @NgModule({
